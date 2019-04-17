b9cefe2e284cb110cc7a47e5cb246a6a92b57ebe
Roberto Maltez <maltez7177@gmail.com>
2019-04-16 23:24:06 -0600

    Primero

---
+++ cmake-build-debug/CMakeFiles/Desing3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Desing3D.dir/main.cpp.o"
+  "Desing3D.pdb"
+  "Desing3D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Desing3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
